3cdef88ef5ac2192a8383b028dc8794aa7973d8e
Gandore <2kidactive@gmail.com>
2025-12-08 15:21:13 -0500

    Day 8

---
+++ 8/partone.rs
@@ -0,0 +1,81 @@
+use std::fs;
+use std::collections::HashMap;
+
+fn main() {
+    let contents = fs::read_to_string("old.txt").expect("");
+    let points: Vec<Vec<u64>> = contents
+        .lines()
+        .map(|x| x
+            .split(",")
+            .map(|y| y.parse::<u64>().unwrap())
+            .collect())
+        .collect();
+
+    // let dists: Vec<(u64, usize, usize)> = points.???():
+    let mut dists: Vec<(u64, usize, usize)> = Vec::new();
+    for (i, point) in points.iter().enumerate() {
+        // let mut min = (u64::MAX, 0, 0);
+        for (j, other) in points[i+1..].iter().enumerate() {
+            let mut sum = 0;
+            for k in 0..3 {
+                sum += point[k].abs_diff(other[k]).pow(2);
+            }
+            // if sum < min.0 {
+                // min = (sum, i, i+j+1);
+            //}
+            dists.push((sum, i, i+j+1));
+        }
+    }
+    dists.sort();
+
+    let boxes: Vec<Vec<usize>> = dists
+        .iter()
+        .map(|(_, x, y)| vec![*x, *y])
+        .collect();
+
+    let mut circuits = HashMap::new();
+
+    for i in 0..10 {
+        // dbg!(circuits.clone());
+        let a = circuits.get(&boxes[i][0]).copied().unwrap_or(0);
+        let b = circuits.get(&boxes[i][1]).copied().unwrap_or(0);
+        if a != 0 && b != 0 {
+            if a == b { continue; }
+            for (key, val) in circuits.clone() {
+                if val == b {
+                    circuits.insert(key, a);
+                }
+            }
+        } else if a != 0 {
+            circuits.insert(boxes[i][1], a);
+        } else if b != 0 {
+            circuits.insert(boxes[i][0], b);
+        } else {
+            circuits.insert(boxes[i][0], i + 1);
+            circuits.insert(boxes[i][1], i + 1);
+        }
+    }
+
+    let mut counts: Vec<(u64, usize)> = Vec::new();
+    for (_, val) in circuits {
+        let mut found = false;
+        for i in 0..counts.len() {
+            if val == counts[i].1 {
+                found = true;
+                counts[i].0 += 1;
+            }
+        }
+        if !found {
+            counts.push((1, val));
+        }
+    }
+
+    counts.sort();
+    counts.reverse();
+
+    let mut product = 1;
+    for i in 0..3 {
+        product *= counts[i].0;
+    }
+    println!("{product}");
+}
